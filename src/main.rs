60e55f881e6b4aee7942b85274705e1d59cd0f89
Tarun Verghis <tarun.verghis@gmail.com>
2024-05-12 09:32:03 -0700

    Add Settings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use bats::settings::Settings;
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let _ = Settings::load().unwrap();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Bats!",
+        native_options,
+        Box::new(|cc| Box::new(BatsApp::new(cc))),
+    )
+    .unwrap();
+}
+
+#[derive(Default)]
+struct BatsApp {}
+
+impl BatsApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for BatsApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |_ui| {});
+    }
 }
---
+++ src/settings.rs
@@ -0,0 +1,56 @@
+use std::{fs::File, io::Read, path::PathBuf};
+
+use serde::{Deserialize, Serialize};
+
+/// Settings that drive Bats functionality
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Settings {
+    /// Directory in which the After Effects applications are installed
+    ae_parent_dir: PathBuf,
+}
+
+impl Settings {
+    /// Load settings from expected locations.
+    /// First, we try to load it from the configuration directory for Bats.
+    /// If that file does not exist, cannot be read, or TOML deserialization fails, then the default settings are used.
+    pub fn load() -> anyhow::Result<Self> {
+        let config_file_path = format!(
+            "{}/Library/Application Support/bats/config.toml",
+            env!("HOME")
+        );
+
+        match File::open(&config_file_path) {
+            Ok(file) => Self::from_config_file_or_default(file),
+            Err(e) => {
+                println!(
+                    "Failed to load config file from {}: {}. Using default settings.",
+                    config_file_path, e
+                );
+                Ok(Self::default())
+            }
+        }
+    }
+
+    fn from_config_file_or_default(mut file: File) -> anyhow::Result<Self> {
+        let mut buf = String::new();
+
+        file.read_to_string(&mut buf)?;
+        match toml::from_str(&buf) {
+            Ok(s) => {
+                println!("Successfully loaded config.toml.");
+                Ok(s)
+            }
+            Err(_) => {
+                println!("Failed to parse config.toml. Using default settings.");
+                Ok(Self::default())
+            }
+        }
+    }
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        let ae_parent_dir = PathBuf::from("/Applications");
+        Self { ae_parent_dir }
+    }
+}
