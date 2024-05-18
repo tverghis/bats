3f50613e5eea6fe057d804238021a33626f3d689
Tarun Verghis <tarun.verghis@gmail.com>
2024-05-17 20:51:20 -0700

    Move old src into src-tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    env_logger::init();
+
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
