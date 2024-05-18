2ce2c699298aed7280d1d628b6fb97f1b64cf3aa
Tarun Verghis <tarun.verghis@gmail.com>
2024-05-17 22:42:12 -0700

    Add bottom StatusBar

--- src-web/src/App.jsx
+++ src-web/src/App.jsx
@@ -1,14 +1,24 @@
 import { invoke } from "@tauri-apps/api";
 import { For, createResource } from "solid-js";
+import StatusBar from "./components/StatusBar";
 
 function App() {
   const [dirs] = createResource(() => invoke("discover_ae_app_dirs"));
 
   return (
     <div className="bg-rosePine-base h-screen text-rosePine-text">
-      {dirs.loading && <p>Loading...</p>}
-      {dirs.error && <p>Error!</p>}
-      {dirs() && <For each={dirs()}>{(dir) => <li>{dir}</li>}</For>}
+      <div className="p-8">
+        {dirs.loading && <p>Loading...</p>}
+        {dirs.error && <p>Error!</p>}
+        {dirs() && (
+          <ul>
+            <For each={dirs()}>{(dir) => <li>{dir}</li>}</For>
+          </ul>
+        )}
+      </div>
+      <div className="fixed bottom-0 left-0 right-0">
+        <StatusBar />
+      </div>
     </div>
   );
 }
---
+++ src-web/src/components/StatusBar.jsx
@@ -0,0 +1,9 @@
+function StatusBar() {
+  return (
+    <div className="py-1 px-3 bg-rosePine-overlay">
+      <p>Down here!</p>
+    </div>
+  );
+}
+
+export default StatusBar;
