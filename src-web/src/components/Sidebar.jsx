dcd27d6eab03ddd314cf5fd2af379f6c7e60e45c
Tarun Verghis <tarun.verghis@gmail.com>
2024-05-20 00:46:55 -0700

    Create basic sidebar

---
+++ src-web/src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { Show } from "solid-js";
+
+function Sidebar(props) {
+  return (
+    <div className="w-20 bg-rosePine-base border-r-rosePine-highlightHigh">
+      <nav>
+        <Show when={props.plugins.length > 0} fallback={<PluginListFallback />}>
+          <PluginList plugins={props.plugins} />
+        </Show>
+      </nav>
+    </div>
+  );
+}
+
+function PluginList(props) {
+  return (
+    <ul role="list" className="flex flex-col items-center space-y-1">
+      <For each={props.plugins}>
+        {(plugin) => {
+          return <li>{plugin}</li>;
+        }}
+      </For>
+    </ul>
+  );
+}
+
+function PluginListFallback() {
+  return <p>No installed plugins</p>;
+}
+
+export default Sidebar;
