4e73381aa608d58e243e8ef6db83691ecffaac22
Tarun Verghis <tarun.verghis@gmail.com>
2024-05-20 14:49:25 -0700

    Reduce sidebar x-padding

--- src-web/src/components/sidebar/Sidebar.jsx
+++ src-web/src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import PluginList from "./PluginList";
 
 function Sidebar(props) {
   return (
-    <div className="w-52 px-8">
+    <div className="w-52 px-6">
       <nav>
         <div className="cursor-pointer">
           <PluginList plugins={props.plugins} />
